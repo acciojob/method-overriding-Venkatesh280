6856baeb6c3e22126f50e03a6c5e4af756188921
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2024-11-14 16:44:27 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-
+ public class Main {
     class A{
         public String meth(){
             return "Invoking method from class A";
@@ -12,8 +12,7 @@ package com.driver;
                 return "Inoking method from class B";
             }
         }
-    
-        public class Main {
+
         public static void main(String args[]){
             B obj=new B();
             System.out.println(obj.meth());
