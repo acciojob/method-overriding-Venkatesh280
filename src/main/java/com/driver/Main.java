17a6ae4b71a78c539bb21ba86149c1682aa804ea
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2024-11-14 16:42:14 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
-public class Main {
+
+    class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+        class B extends A{
+            @Override
+            public String meth(){
+                return "Inoking method from class B";
+            }
+        }
+    
+        public class Main {
+        public static void main(String args[]){
+            B obj=new B();
+            System.out.println(obj.meth());
+
+        }
+    }
   
-}
